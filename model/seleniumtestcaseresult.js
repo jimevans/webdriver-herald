8e25150ef5c3ec1a6cce8e394b93e4cbe7ef61d1
Jim Evans <james.h.evans.jr@gmail.com>
2019-06-13 13:52:33 -0700

    Updating Selenium model to account for tests run with Bazel

--- model/seleniumtestcaseresult.js
+++ model/seleniumtestcaseresult.js
@@ -1,18 +1,51 @@
 module.exports = class SeleniumTestCaseResult {
   constructor(resultObject) {
-    this.status = resultObject['@result'].toLowerCase();
-    this.fullname = resultObject['@fullname'];
-    this.modulename = resultObject['@classname'];
-    this.testname = resultObject['@methodname'];
-    if (this.status == 'skipped') {
-      var properties = resultObject['properties'];
-      if (properties != null) {
-        this.reason = properties['property']['@value'];
-      } else {
-        this.reason = resultObject['reason']['message']['#cdata-section'];
+    if (resultObject['@result']) {
+      this.status = resultObject['@result'].toLowerCase();
+    } else if (resultObject['@status']) {
+      this.status = resultObject['@status'].toLowerCase();
+    } else {
+      throw "No status attribute ('@result' or '@status') found in test result.";
+    }
+
+    if (resultObject['@methodname']) {
+      this.testname = resultObject['@methodname'];
+    } else if (resultObject['@name']) {
+      this.testname = resultObject['@name'];
+    } else {
+      throw "No test name attribute ('@methodname' or '@name') found in test result.";
+    }
+
+    if (resultObject['@classname']) {
+      this.modulename = resultObject['@classname'];
+    } else {
+      throw "No test class attribute ('@classname') found in test result.";
+    }
+
+    if (resultObject['@fullname']) {
+      this.fullname = resultObject['@fullname'];
+    } else {
+      this.fullname = this.modulename + '.' + this.testname;
+    }
+
+    if (this.status === 'skipped') {
+      if (resultObject['properties']) {
+        var properties = resultObject['properties'];
+        if (properties !== null) {
+          this.reason = properties['property']['@value'];
+        } else {
+          this.reason = resultObject['reason']['message']['#cdata-section'];
+        }
+      } else if (resultObject['skipped']) {
+        this.reason = resultObject['skipped'][1]['@message'];
       }
-    } else if (this.status == 'failed') {
-      this.reason = resultObject['failure']['message']['#cdata-section'];
+    } else if (this.status === 'failed') {
+      var failureObject = resultObject['failure'];
+      if (failureObject['message']) {
+        this.reason = failureObject['message']['#cdata-section'].trim();
+      } else {
+        this.reason = failureObject['@message'].trim();
+      }
     } else {
       this.reason = 'passed';
     }
--- model/seleniumtestresults.js
+++ model/seleniumtestresults.js
@@ -2,49 +2,75 @@ const SeleniumTestCaseResult = require('./seleniumtestcaseresult.js')
 
 module.exports = class SeleniumTestResults {
   constructor(browserInfo, testRunData) {
-    var assemblySuites = testRunData['test-run']['test-suite'];
-    for (var i = 0; i < assemblySuites.length; i++) {
-      var testSuiteSummary = assemblySuites[i];
-      if (testSuiteSummary['@name'] === 'WebDriver.Common.Tests.dll') {
-        this.description = browserInfo['description'];
-        this.browser = browserInfo['browser'];
-        this.driver = browserInfo['driver'];
-        this.total = testSuiteSummary['@testcasecount'];
-        this.passed = testSuiteSummary['@passed'];
-        this.failed = testSuiteSummary['@failed'];
-        this.skipped = testSuiteSummary['@skipped'];
-        this.duration = (testSuiteSummary['@duration'] * 1).toFixed(3);
-        this.passrate = (this.passed / (this.total - this.skipped) * 100).toFixed(2);
-
-        function processTestResults(run) {
-          var resultArray = [];
-          for (var i = 0; i < run.length; i++) {
-            var testClass = run[i];
-            var testClassType = testClass['@type'];
-            if (testClassType == 'TestFixture') {
-              var count = parseInt(testClass['@testcasecount']);
-              if (count == 1) {
-                var testCase = testClass['test-case'];
-                resultArray.push(new SeleniumTestCaseResult(testCase));
-              } else {
-                var testMethods = testClass['test-case'];
-                if (testMethods) {
-                  for (var j = 0; j < testMethods.length; j++) {
-                    resultArray.push(new SeleniumTestCaseResult(testMethods[j]));
-                  }
-                }
+    function processTestResults(run) {
+      var resultArray = [];
+      for (var i = 0; i < run.length; i++) {
+        var testClass = run[i];
+        var testClassType = testClass['@type'];
+        if (testClassType === 'TestFixture') {
+          var count = parseInt(testClass['@testcasecount']);
+          if (count === 1) {
+            var testCase = testClass['test-case'];
+            resultArray.push(new SeleniumTestCaseResult(testCase));
+          } else {
+            var testMethods = testClass['test-case'];
+            if (testMethods) {
+              for (var j = 0; j < testMethods.length; j++) {
+                resultArray.push(new SeleniumTestCaseResult(testMethods[j]));
               }
-            } else if (testClassType == 'TestSuite') {
-              var interimArray = processTestResults(testClass['test-suite']);
-              Array.prototype.push.apply(resultArray, interimArray);
             }
           }
-          return resultArray;
-        };
-
-        var run = testSuiteSummary['test-suite']['test-suite']['test-suite'];
-        this.tests = processTestResults(run);
+        } else if (testClassType === 'TestSuite') {
+          var interimArray = processTestResults(testClass['test-suite']);
+          Array.prototype.push.apply(resultArray, interimArray);
+        }
       }
+      return resultArray;
+    }
+
+    function processBazelTestResults(run) {
+      var resultArray = [];
+      var testSuites = testRun['testsuite'];
+      for (var testSuiteIndex = 0; testSuiteIndex < testSuites.length; testSuiteIndex++) {
+        var testSuite = testSuites[testSuiteIndex];
+        var testCases = testSuite['testcase'];
+        for (var testCaseIndex = 0; testCaseIndex < testCases.length; testCaseIndex++) {
+          var testCase = testCases[testCaseIndex];
+          resultArray.push(new SeleniumTestCaseResult(testCase));
+        }
+      }
+      return resultArray;
+    }
+
+    this.description = browserInfo['description'];
+    this.browser = browserInfo['browser'];
+    this.driver = browserInfo['driver'];
+    if (testRunData['test-run']) {
+      var assemblySuites = testRunData['test-run']['test-suite'];
+      for (var i = 0; i < assemblySuites.length; i++) {
+        var testSuiteSummary = assemblySuites[i];
+        if (testSuiteSummary['@name'] === 'WebDriver.Common.Tests.dll') {
+          this.total = testSuiteSummary['@testcasecount'];
+          this.passed = testSuiteSummary['@passed'];
+          this.failed = testSuiteSummary['@failed'];
+          this.skipped = testSuiteSummary['@skipped'];
+          this.duration = (testSuiteSummary['@duration'] * 1).toFixed(3);
+          this.passrate = (this.passed / (this.total - this.skipped) * 100).toFixed(2);
+
+          var run = testSuiteSummary['test-suite']['test-suite']['test-suite'];
+          this.tests = processTestResults(run);
+        }
+      }
+    } else {
+      var testRun = testRunData['testsuites'];
+      this.total = parseInt(testRun['@tests']);
+      this.failed = parseInt(testRun['@failures']);
+      this.skipped = parseInt(testRun['@disabled']);
+      this.duration = (testRun['@time'] * 1).toFixed(3);
+      this.passed = this.total - (this.failed + this.skipped);
+      this.passrate = (this.passed / (this.total - this.skipped) * 100).toFixed(2);
+      var testSuites = testRun['testsuite'];
+      this.tests = processBazelTestResults(testSuites);
     }
   }
 };
